66e7309f7a843417586911676eb6e61569d480d7
winter2012 <winter2012102@gmail.com>
2019-03-29 22:09:06 +0800

    添加@yield('content')以及测试页面

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -2,7 +2,10 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Starter Page</h1>
+                <h1 class="m-0 text-dark">
+                    {{ $page_title or "Page Title" }}
+                    <small>{{ $page_description or null }}</small>
+                </h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('admin', function () {
     return view('admin_template');
 });
+
+//超级管理员
+Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' =>	'auth'],function(){
+
+    Route::get('test', 'TestController@index');
+
+});
