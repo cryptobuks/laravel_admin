49859ee27b083d109af855fd6558762b8de9552b
winter2012 <winter2012102@gmail.com>
2019-03-30 10:00:00 +0800

    tiny

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,6 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
+/**
+ * @var \Illuminate\Database\Eloquent\Factory $factory
+ */
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
