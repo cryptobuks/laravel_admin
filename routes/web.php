5887681afbf71c8cd1429623b49af8941392f78c
winter2012 <winter2012102@gmail.com>
2019-03-30 17:42:23 +0800

    所有路由都经过auth中间件

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 //超级管理员
 Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' =>	'auth'],function(){
