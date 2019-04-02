d7808b8b0e1366ca44021634a5ff1a14f0835af0
winter2012 <winter2012102@gmail.com>
2019-04-02 11:20:15 +0800

    sidebar->menu

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -23,9 +23,9 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview menu-open">
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link active">
-                        <i class="nav-icon fa fa-dashboard"></i>
+                        <i class="nav-icon fa fa-table"></i>
                         <p>
                             Starter Pages
                             <i class="right fa fa-angle-left"></i>
@@ -46,7 +46,7 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item">
+                {{--<li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fa fa-th"></i>
                         <p>
@@ -54,7 +54,7 @@
                             <span class="right badge badge-danger">New</span>
                         </p>
                     </a>
-                </li>
+                </li>--}}
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
