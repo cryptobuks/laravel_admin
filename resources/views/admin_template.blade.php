620b191b592ece10c920818059bc9990a8383950
winter2012 <winter2012102@gmail.com>
2019-04-03 13:33:20 +0800

    更换favicon图标

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <link rel="icon" type="image/x-icon" href="https://github.githubassets.com/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/bower_components/admin-lte/dist/img/google_wallet.ico">
     <title>管理后台</title>
 
     <!-- Font Awesome Icons -->
