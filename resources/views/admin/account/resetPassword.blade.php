53a4f70fe5cd72614df1f2ec9bc48f953c8a5ab4
winter2012 <winter2012102@gmail.com>
2019-04-05 09:36:47 +0800

    resetPassword优化

--- resources/views/admin/account/resetPassword.blade.php
+++ resources/views/admin/account/resetPassword.blade.php
@@ -8,28 +8,25 @@
                     <!-- general form elements -->
                     <div class="card card-primary">
                         <!-- /.card-header -->
-                        <!-- form start -->
-                        <form role="form" method="POST">
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="oldPassword">旧密码</label>
-                                    <input type="password" class="form-control" name="oldPassword" id="oldPassword" placeholder="请输入旧密码" required autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <label for="newPassword">新密码</label>
-                                    <input type="password" class="form-control" name="newPassword" id="newPassword" placeholder="请输入新密码" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="confirmPassword">确认密码</label>
-                                    <input type="password" class="form-control" name="newPassword_confirmation" id="newPassword_confirmation" placeholder="请输入确认密码" required>
-                                </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="oldPassword">旧密码</label>
+                                <input type="password" class="form-control" name="oldPassword" placeholder="请输入旧密码" required autofocus>
                             </div>
-                            <!-- /.card-body -->
+                            <div class="form-group">
+                                <label for="newPassword">新密码</label>
+                                <input type="password" class="form-control" name="newPassword" placeholder="请输入新密码" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="confirmPassword">确认密码</label>
+                                <input type="password" class="form-control" name="newPassword_confirmation" placeholder="请输入确认密码" required>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
 
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">提交修改</button>
-                            </div>
-                        </form>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">提交修改</button>
+                        </div>
                     </div>
                     <!-- /.card -->
                 </div>
@@ -42,16 +39,16 @@
     <script type="text/javascript">
         var $j = jQuery.noConflict();
         $j(':submit').click(function(){
-            event.preventDefault();
             $j.ajax({
                 url : "/admin/account/password",
                 type : "post",
-                data : $j('form').serialize(),
+                data : $j(':password').serialize(),
                 success: function(data){
                     console.log(data);
                     if(data.status === 0){
                         alert(data.message);
                         window.location.reload();
+                        // location.href = "/";
                     }else{
                         alert(data.message);
                     }
