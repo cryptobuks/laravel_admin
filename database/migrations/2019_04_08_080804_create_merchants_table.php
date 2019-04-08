b62572de8d6c0676649dca19d7190b2a305a9b92
winter2012 <winter2012102@gmail.com>
2019-04-08 16:40:25 +0800

    php artisan make:migration create_merchants_table

---
+++ database/migrations/2019_04_08_080804_create_merchants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMerchantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('merchant_no');
+            $table->string('name');
+            $table->string('password');
+            $table->string('security_password');
+            $table->string('key');
+            $table->string('salt');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+}
