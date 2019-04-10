f0e3e52343fa8708568d3b182871d7611a653f96
winter2012 <winter2012102@gmail.com>
2019-04-11 02:10:25 +0800

    tiny

--- config/constants.php
+++ config/constants.php
@@ -1,29 +1,31 @@
 <?php
 return [
     'admin/index'               => '首页',
+
     'admin/account/index'       => '管理员列表',
     'admin/account/password'    => '修改密码页面',
     'admin/account/create'      => '添加管理员',
     'admin/account/edit'        => '编辑管理员',
     'admin/account/reset'       => '重置管理员密码',
     'admin/account/del'         => '删除管理员',
+
     'admin/merchant/index'      => '商户列表',
     'admin/merchant/create'     => '添加商户',
     'admin/merchant/key'        => '重置商户密钥',
     'admin/merchant/password'   => '重置商户登录密码',
     'admin/merchant/security'   => '重置商户资金密码',
     'admin/merchant/del'        => '删除商户',
+
     'admin/permission/index'    => '权限列表',
     'admin/permission/clear'    => '清除权限缓存',
     'admin/permission/restore'  => '重置权限表',
     'admin/permission/edit'     => '编辑权限',
     'admin/permission/del'      => '删除权限',
+
     'admin/menu/index'          => '菜单列表',
     'admin/menu/create'         => '添加菜单',
     'admin/menu/edit'           => '修改菜单',
     'admin/menu/del'            => '删除菜单',
-
-
 
     'admin/test'                => '调试页面',
 ];
