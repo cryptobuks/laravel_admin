773a25d2b05373b33747120e9f7d00a4fbcef5ac
James <James@gmail.com>
2019-04-12 18:02:18 +0800

    php artisan make:seeder PermissionsTableSeeder

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permissions')->truncate();
+
+        //重构权限表
+
+    }
+}
